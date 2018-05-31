8a9f657da307b8c0f6231b1e5ece3c0b067ee1e1
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-31 18:25:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeli = [];
+function takeANumber (katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+function nowServing (katzDeli) {
+  //return first person in line then remove them from the line
+  //if no one is in line then it should say something else (use if/ else statment)
+let i = 0;
+    while (i < katzDeli.length) {
+      i++;
+    }
+      if (katzDeli.length === 0) {
+    return  'There is nobody waiting to be served!';
+  }
+    else
+    return (`Currently serving ${katzDeli.shift()}.`);
+   }
+
+function currentLine (katzDeli) {
+  let i = 0;
+  let line = "The line is currently:"
+    i++
+  while (i < katzDeli.length) {
+    line  += (i + 1) + ". " + katzDeli[i] + ", "
+    //show who is in line when multiple people are in line
+  }
+  if (katzDeli.length === 0) {
+return  ('The line is currently empty.') ;
+}
+else
+return line ;
+}
